22604232d3dc71dd3f0b106c42a0f9682f46ec96
bab00n <yurinikiforov8@gmail.com>
2025-03-05 21:26:39 +0500

    fafawfawf

--- нов-жсон/project/server/server.js
+++ нов-жсон/project/server/server.js
@@ -7,7 +7,6 @@ const querystring = require('querystring');
 process.stdout.setEncoding('utf8');
 process.stdin.setEncoding('utf8');
 
-// Функции для работы с данными пользователей
 function getUsers() {
     const data = fs.readFileSync(path.join(__dirname, 'users.json'), 'utf-8');
     return JSON.parse(data);
@@ -21,8 +20,10 @@ const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
     const pathname = parsedUrl.pathname;
 
+    console.log('Requested Path:', pathname);
+
     if (pathname === '/' && req.method === 'GET') {
-        fs.readFile(path.join(__dirname, '..', 'client', 'public', 'index.html'), 'utf-8', (err, data) => {
+        fs.readFile(path.join(__dirname, 'index.html'), 'utf-8', (err, data) => {
             if (err) {
                 console.log(err); 
                 res.statusCode = 500;
@@ -34,88 +35,130 @@ const server = http.createServer((req, res) => {
             res.end(data);
         });
     }
-    
-   // Обработка логина
-else if (pathname === '/index' && req.method === 'POST') {
-    let body = '';
-    req.on('data', chunk => {
-        body += chunk;
-    });
-    req.on('end', () => {
-        const { username, password } = querystring.parse(body);
-        const users = getUsers();
 
-        const user = users.find(user => user.username === username && user.password === password);
+    else if (pathname === '/index' && req.method === 'POST') {
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk;
+        });
+        req.on('end', () => {
+            const { username, password } = querystring.parse(body);
+            const users = getUsers();
 
-        res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
-        if (user) {
-            res.statusCode = 302; // Код для редиректа
-            res.setHeader('Location', '/home'); // Перенаправление на домашнюю страницу (или другую страницу)
-            res.end();
-        } else {
-            res.statusCode = 401;
-            res.end('Неверный логин или пароль');
-        }
-    });
-}
+            const user = users.find(user => user.username === username && user.password === password);
 
-    // Обработка регистрации
-    // Обработка регистрации
-else if (pathname === '/register' && req.method === 'POST') {
-    let body = '';
-    req.on('data', chunk => {
-        body += chunk;
-    });
-    req.on('end', () => {
-        const { username, password } = querystring.parse(body);
-        const users = getUsers();
-
-        const userExists = users.find(user => user.username === username);
-        if (userExists) {
             res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
-            res.statusCode = 409; 
-            res.end('Пользователь с таким именем уже существует');
-            return;
-        }
-
-        users.push({ username, password });
-        saveUsers(users);
-
-        res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
-        res.statusCode = 302; // Код для редиректа
-        res.setHeader('Location', '/index'); // Перенаправление на страницу входа после успешной регистрации
-        res.end();
-    });
-}
-
-    // Обслуживание статичных файлов (css, js, изображения)
-    else {
-        const staticFilePath = path.join(__dirname, '..', 'client', 'public', pathname);
-        fs.readFile(staticFilePath, (err, data) => {
-            if (err) {
-                console.log("Ошибка при чтении статического файла:", err); // Логирование ошибок
-                res.statusCode = 404;
-                res.end('Не найдено');
+            if (user) {
+                res.statusCode = 302; 
+                res.setHeader('Location', '/home'); 
+                res.end();
             } else {
-                res.statusCode = 200;
-                const extname = path.extname(pathname);
-                let contentType = 'text/plain';
-
-                if (extname === '.html') contentType = 'text/html; charset=UTF-8';
-                if (extname === '.css') contentType = 'text/css';
-                if (extname === '.js') contentType = 'application/javascript';
-                if (extname === '.png') contentType = 'image/png';
-                if (extname === '.jpg' || extname === '.jpeg') contentType = 'image/jpeg';
-                if (extname === '.gif') contentType = 'image/gif';
-
-                res.setHeader('Content-Type', contentType);
-                res.end(data);
+                res.statusCode = 401;
+                res.end('Неверный логин или пароль');
             }
         });
     }
+
+    else if (pathname === '/register' && req.method === 'POST') {
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk;
+        });
+        req.on('end', () => {
+            const { username, password } = querystring.parse(body);
+            const users = getUsers();
+
+            const userExists = users.find(user => user.username === username);
+            if (userExists) {
+                res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
+                res.statusCode = 409; 
+                res.end('Пользователь с таким именем уже существует');
+                return;
+            }
+
+            users.push({ username, password });
+            saveUsers(users);
+
+            res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
+            res.statusCode = 302; 
+            res.setHeader('Location', '/home'); 
+            res.end();
+        });
+    }
+
+    else if (pathname === '/home' && req.method === 'GET') {
+        fs.readFile(path.join(__dirname, 'home.html'), 'utf-8', (err, data) => {
+            if (err) {
+                console.log("Ошибка при загрузке страницы:", err);
+                res.statusCode = 500;
+                res.end('Ошибка сервера');
+                return;
+            }
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+            res.end(data);
+        });
+    }
+
+    else {
+        const staticFilePath = path.join(__dirname, pathname);
+        
+        console.log('Запрашиваем статический файл:', staticFilePath);
+    
+
+        fs.exists(staticFilePath, (exists) => {
+            if (!exists) {
+                console.log(`Файл не найден: ${staticFilePath}`);
+                res.statusCode = 404;
+                res.end('Не найдено');
+                return;
+            }
+    
+            fs.readFile(staticFilePath, (err, data) => {
+                if (err) {
+                    console.log("Ошибка при чтении файла:", err);
+                    res.statusCode = 500;
+                    res.end('Ошибка сервера');
+                    return;
+                }
+
+                const extname = path.extname(pathname);
+                let contentType = 'text/plain';
+    
+                switch (extname) {
+                    case '.html':
+                        contentType = 'text/html; charset=UTF-8';
+                        break;
+                    case '.css':
+                        contentType = 'text/css';
+                        break;
+                    case '.js':
+                        contentType = 'application/javascript';
+                        break;
+                    case '.png':
+                        contentType = 'image/png';
+                        break;
+                    case '.jpg':
+                    case '.jpeg':
+                        contentType = 'image/jpeg';
+                        break;
+                    case '.gif':
+                        contentType = 'image/gif';
+                        break;
+                    default:
+                        contentType = 'text/plain';
+                        break;
+                }
+
+                res.statusCode = 200;
+                res.setHeader('Content-Type', contentType);
+                res.end(data);
+            });
+        });
+    }
+    
 });
 
-// Запуск сервера на порту 3000
 const port = 3000;
 server.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
